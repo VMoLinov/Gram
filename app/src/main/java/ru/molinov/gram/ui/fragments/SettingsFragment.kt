b0d3d8fbc2cd8c68367f864632f06e2bd9ca21dd
Molinov <molinov@list.ru>
2022-07-14 14:07:23 +0300

    Settings action menu

--- app/src/main/java/ru/molinov/gram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ru/molinov/gram/ui/fragments/SettingsFragment.kt
@@ -1,6 +1,18 @@
 package ru.molinov.gram.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
+import ru.molinov.gram.R
 import ru.molinov.gram.databinding.FragmentSettingsBinding
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
+
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        requireActivity().menuInflater.inflate(R.menu.settings_action_menu, menu)
+    }
 }
